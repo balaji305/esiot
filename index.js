c4ad3a161bcd3ca33691c5db008109c76fa6b4fc
Balaji <balajisankar555@gmail.com>
2023-05-12 11:16:02 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/iot', (req, res) => {
+  const fs = require('fs');
+ 
+// Calling the readFileSync() method
+// to read 'input.txt' file
+const data = fs.readFileSync('./ans.txt',
+    { encoding: 'utf8', flag: 'r' });
+    res.send(data);
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
