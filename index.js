6bfbd1b894908d65a1aeb5c697b7a6714a666a7f
Balaji <balajisankar555@gmail.com>
2023-11-05 18:24:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,34 +2,103 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/iot', (req, res) => {
+app.get('/hill', (req, res) => {
   const fs = require('fs');
-  const data = fs.readFileSync('./ans.txt',
+  const data = fs.readFileSync('./hill.txt',
+    { encoding: 'utf8', flag: 'r' });
+  res.send(data);
+})
+
+app.get('/des', (req, res) => {
+  const fs = require('fs');
+  const data = fs.readFileSync('./des.txt',
     { encoding: 'utf8', flag: 'r' });
    res.send(data);
 })
 
-app.get('/keil', (req, res) => {
+app.get('/aes', (req, res) => {
   const fs = require('fs');
-  const data = fs.readFileSync('./timer.txt',
+  const data = fs.readFileSync('./aes.txt',
+    { encoding: 'utf8', flag: 'r' });
+   res.send(data);
+})
+
+app.get('/rsa', (req, res) => {
+  const fs = require('fs');
+  const data = fs.readFileSync('./rsa.txt',
     { encoding: 'utf8', flag: 'r' });
   res.send(data);
 })
 
-app.get('/mongo', (req, res) => {
+app.get('/hellman', (req, res) => {
   const fs = require('fs');
-  const data = fs.readFileSync('./mongo.txt',
+  const data = fs.readFileSync('./hellman.txt',
     { encoding: 'utf8', flag: 'r' });
   res.send(data);
 })
 
-app.get('/hadoop', (req, res) => {
+app.get('/elg', (req, res) => {
   const fs = require('fs');
-  const data = fs.readFileSync('./hadoop.txt',
+  const data = fs.readFileSync('./elg.txt',
     { encoding: 'utf8', flag: 'r' });
   res.send(data);
 })
 
+app.get('/sha', (req, res) => {
+  const fs = require('fs');
+  const data = fs.readFileSync('./sha.txt',
+    { encoding: 'utf8', flag: 'r' });
+  res.send(data);
+})
+
+app.get('/md5', (req, res) => {
+  const fs = require('fs');
+  const data = fs.readFileSync('./md5.txt',
+    { encoding: 'utf8', flag: 'r' });
+  res.send(data);
+})
+
+app.get('/hillattack', (req, res) => {
+  const fs = require('fs');
+  const data = fs.readFileSync('./hillattack.txt',
+    { encoding: 'utf8', flag: 'r' });
+  res.send(data);
+})
+
+app.get('/affineattack', (req, res) => {
+  const fs = require('fs');
+  const data = fs.readFileSync('./affineattack.txt',
+    { encoding: 'utf8', flag: 'r' });
+  res.send(data);
+})
+
+app.get('/andriod1', (req, res) => {
+  const fs = require('fs');
+  const data = fs.readFileSync('./andriod1.txt',
+    { encoding: 'utf8', flag: 'r' });
+  res.send(data);
+})
+
+app.get('/andriod2', (req, res) => {
+  const fs = require('fs');
+  const data = fs.readFileSync('./andriod2.txt',
+    { encoding: 'utf8', flag: 'r' });
+  res.send(data);
+})
+
+app.get('/andriod3', (req, res) => {
+  const fs = require('fs');
+  const data = fs.readFileSync('./andriod3.txt',
+    { encoding: 'utf8', flag: 'r' });
+  res.send(data);
+})
+
+app.get('/andriod4', (req, res) => {
+  const fs = require('fs');
+  const data = fs.readFileSync('./andriod4.txt',
+    { encoding: 'utf8', flag: 'r' });
+  res.send(data);
+})
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
